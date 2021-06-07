1299cfea8c51980b3c3c8eb65a5a5308aa5d9f3b
Natashalysaker <55975952+Natashalysaker@users.noreply.github.com>
2021-06-07 12:23:01 +0200

    dark purple font

--- components/intro.js
+++ components/intro.js
@@ -2,13 +2,13 @@ export default function Intro() {
   return (
     <section className="h-screen bg- flex  items-center">
       <div className=" flex flex-col justify-between w-full md:w-3/4  ">
-        <h3 className="font-rob font-medium text-3xl mx-4 sm:mx-1 md:mx-20 lg:m-44 xl:mx-56">
+        <h3 className="font-rob font-col font-medium text-3xl mx-4 sm:mx-1 md:mx-20 lg:m-44 xl:mx-56">
           Hi there
         </h3>
-        <h2 className="font-rob font-normal text-4xl mx-4 sm:mx-1 md:mx-20 lg:m-44 xl:mx-56">
+        <h2 className="font-rob font-col font-normal text-4xl mx-4 sm:mx-1 md:mx-20 lg:m-44 xl:mx-56">
           My name is Natasha Lysaker
         </h2>
-        <h1 className="font-rob text-left text-6xl md: lg:text-7xl tracking-tighter leading-tight md:pr-8 mx-4 sm:mx-1 md:mx-20 lg:m-44 xl:mx-56">
+        <h1 className="font-rob font-col text-left text-6xl md: lg:text-7xl tracking-tighter leading-tight md:pr-8 mx-4 sm:mx-1 md:mx-20 lg:m-44 xl:mx-56">
           I am a <span className="font-semibold">front-end develuper</span> and
           a <span className="font-semibold">ux-designer</span> from Norway
         </h1>
